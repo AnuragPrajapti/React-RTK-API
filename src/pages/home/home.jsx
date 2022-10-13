808ed1220b1821b6d8dd78eea5331b7293b806db
Anurag Prajapati <anuragyuvasoft386@gmail.com>
2022-10-13 11:07:54 +0530

    add a image api

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { HeadingHomePage , ImageWrapper } from './homeStyle';
+import axios from 'axios';
 
 const Home = () => {
+   
+   const [ imageApiData , setImageApiData ] = useState();
+
+   axios 
+      .get(`https://629d9b8fc6ef9335c0a0e2d5.mockapi.io/fake_api`).then((res)=>{
+           console.log("data",res.data);
+           setImageApiData(res.data);
+      }).catch((err)=>{
+          console.log(err)
+      })
+
   return (
-    <div>home</div>
+    <div className='container'>
+       <HeadingHomePage>List of Image Api!!!</HeadingHomePage>
+       {
+          imageApiData?.map((item,index)=> <div key={index}>
+                    <ImageWrapper>
+                     <img src={item.avatar}/>
+                     <p>{item.name}</p>
+                     </ImageWrapper>
+          </div> )
+       }            
+    </div>
   )
 }
 
